51b5d91d2336f9ee55eeb58e2b255ea12e5e8761
Kevin Ahn <kevinahn7@hotmail.com>
2018-06-08 11:18:56 -0700

    Add ability to add users name to the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 //Back end, Business logic
 var array = [];
-function beepBoop(input) {
+function beepBoop(input, name) {
     if (isNaN(input)) {
-        var invalidNumber = "That is not a valid input, please enter a number.";
+        var invalidNumber = "That is not a valid input " + name + ", please enter a number.";
         array.push(invalidNumber);
         return array;
     } else {
         for (x=0; x<= input; x++) {
             var pushIt = x;
             if (x % 3 === 0 && x !== 0) {
-                pushIt = "I'm sorry, Bob. I'm afraid I can't do that.";
+                if (name !== "") {
+                    pushIt = "I'm sorry, " + name + ". I'm afraid I can't do that.";
+                } else {
+                    pushIt = "I'm sorry. I'm afraid I can't do that.";
+                }
             } else if (x.toString().split("").includes("1")) {
                 pushIt = "Boop!";
             } else if (x.toString().split("").includes("0")) {
@@ -25,9 +29,11 @@ $(document).ready(function() {
     $("#theForm").submit(function(event) {
         event.preventDefault();
         array = [];
+        var userName = $(".namer").val();
         var numberString = $("#inputNumber").val();
         var numberInt = parseInt(numberString);
-        var answer = beepBoop(numberInt);
+        var answer = beepBoop(numberInt, userName);
         $(".answer").text(answer);
+        console.log(userName)
     });
 });
